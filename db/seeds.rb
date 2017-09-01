abe5cac4c77f7030035608a1e6d7d235716ddae4
elijah <eli.heineman@gmail.com>
2017-09-01 16:55:06 +0000

    added review table

--- db/seeds.rb
+++ db/seeds.rb
@@ -132,5 +132,34 @@ cat3.products.create!({
   price: 2_483.75
 })
 
+puts 'Creating Reviews'
+
+Review.create!({
+ product_id: 1,
+ user_id: 1,
+ description: 'wack',
+ rating: 1
+})
+
+Review.create!({
+ product_id: 2,
+ user_id: 1,
+ description: 'trash',
+ rating: 2
+})
+
+Review.create!({
+ product_id: 3,
+ user_id: 1,
+ description: 'not good',
+ rating: 3
+})
+
+Review.create!({
+ product_id: 4,
+ user_id: 1,
+ description: 'lame',
+ rating: 4
+})
 
 puts "DONE!"
