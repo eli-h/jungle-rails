2c325619e7a6c5d73f47f0c963bdedee4ae569d5
elijah <eli.heineman@gmail.com>
2017-09-03 16:36:15 +0000

    added delete review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,10 +1,12 @@
 class ReviewsController < ApplicationController
+  
+  before_filter :authorize
 
   def create
   	@product = Product.find(params[:product_id])
     @review = Review.new(review_params)
     @review.product_id = params[:product_id]
-    @review.user_id = current_user.name
+    @review.user_id = current_user
     @review.save
 
     if @review.save
@@ -14,11 +16,25 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+  	@review = Review.find(params[:id])
+  	@review.destroy
+  	redirect_to [:products], notice: 'Review deleted'
+  end
+
   def review_params
     params.require(:review).permit(
       :description,
       :rating
     )
   end
+
+  # private
+
+  # def user_auth
+  #   unless current_user
+  #     @review = @product.reviews.new(review_params)
+  #   end
+  # end
 
 end
